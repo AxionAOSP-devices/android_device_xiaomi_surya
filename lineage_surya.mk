5a827a4ba8235c4d8cd829da59a5d40eabe2e48b
HinohArata <mmgcntk@gmail.com>
2025-06-25 01:58:46 +0700

    surya: Disable Enhanced Power Policy Engine (EPPE)
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -13,6 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Axion Flags
