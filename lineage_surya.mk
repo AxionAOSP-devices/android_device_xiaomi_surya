419c854444645124e5f61482d74c787a9e8939d3
HinohArata <mmgcntk@gmail.com>
2025-06-24 10:42:59 +0700

    surya: Axionify Enabled flags: - Bypass Charge - Include los prebuilts for GMS variant - Enable blur
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -15,6 +15,35 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Axion Flags
+AXION_MAINTAINER := Skyy丨アラタ
+AXION_PROCESSOR := Qualcomm®_Snapdragon_732G
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Bypass charge
+BYPASS_CHARGE_SUPPORTED := true
+
+# Camera
+AXION_CAMERA_REAR_INFO := 64,13,2,2
+AXION_CAMERA_FRONT_INFO := 20
+
+# CPU
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+AXION_CPU_BG := 0-1
+AXION_CPU_FG := 0-7
+
+# GApps
+WITH_GMS := true
+
+# Los prebuilts
+ifneq ($(WITH_GMS),true)
+TARGET_INCLUDES_LOS_PREBUILTS := true
+endif
+
+# Device manufacturer
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
